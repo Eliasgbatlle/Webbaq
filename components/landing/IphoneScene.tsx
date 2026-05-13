7c9c85094705932526821c11cbc843e182cb9cf5
[dyad] <git@dyad.sh>
2026-05-13 18:26:38 -0500

    [dyad] Invirtiendo la dirección del scroll en el iPhone - wrote 1 file(s)

--- components/landing/IphoneScene.tsx
+++ components/landing/IphoneScene.tsx
@@ -48,11 +48,8 @@ function Model(props: any) {
 
   // ANIMACIONES
   useFrame((state, delta) => {
-    // Animación de scroll en la textura
-    texture.offset.y -= delta * 0.05;
-    if (texture.offset.y < -1) {
-      texture.offset.y = 0;
-    }
+    // Animación de scroll en la textura (de abajo hacia arriba)
+    texture.offset.y = (texture.offset.y + delta * 0.05) % 1;
 
     if (groupRef.current) {
       // Animación sutil de flotación vertical
