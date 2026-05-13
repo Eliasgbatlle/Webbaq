8976f95fdda52ba8754d873265a56b40a2bb89e8
[dyad] <git@dyad.sh>
2026-05-13 16:43:15 -0500

    Reverted all changes back to version f2327e21682001809873b45f816e4d054158b157

--- components/landing/IphoneScene.tsx
+++ components/landing/IphoneScene.tsx
@@ -36,11 +36,8 @@ function Model(props: any) {
   }, [screenTexture]);
 
   // Create the material for the screen
-  const screenMaterial = new THREE.MeshStandardMaterial({
+  const screenMaterial = new THREE.MeshBasicMaterial({
     map: screenTexture,
-    emissiveMap: screenTexture,
-    emissive: 'white',
-    emissiveIntensity: 0.5,
     toneMapped: false,
   });
 
